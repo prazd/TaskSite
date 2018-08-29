106bb365b94130f4728822df07f72942d30a9ceb
prazd <alexokda@list.ru>
2018-08-29 14:33:29 +0300

    site

--- Page/Project/Project/js/script.js
+++
@@ -1,6 +0,0 @@
-  var menuElem = document.getElementById('nav');
-    var titleElem = menuElem.querySelector('.icon');
-
-    titleElem.onclick = function() {
-      menuElem.classList.toggle('open');
-    };
